ba1e917ca5bc66aaab26b4c7c3bae63b78be7b0f
Nimrod <nimrod@wear-2b.com>
2022-07-07 14:18:16 +0300

    create test repo

---
+++ test/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name='test',
+    version='0.0.1',
+    author='nimrod wear-2b',
+    author_email='nimrod@wear-2b.com',
+    description='Testing installation of Package',
+    # long_description=long_description,
+    # long_description_content_type="text/markdown",
+    url='https://github.com/mike-huls/toolbox',
+    license='None',
+    packages=['test'],
+    install_requires=['struct'],
+)
